d44fe0320bb736f1d6871369ab6d800a3b726f6c
Victor A. <52110451+cs50victor@users.noreply.github.com>
2026-03-23 10:09:50 -0700

    fix: revise MCP trust post and add blockquote styling

--- app/t/layout.tsx
+++ app/t/layout.tsx
@@ -37,7 +37,7 @@ export default function LayoutBlogPost({
       <div className="absolute right-4 top-24">
         <CopyButton />
       </div>
-      <main className="mb-52 prose prose-gray md:prose-figure:-mx-18 lg:prose-figure:-mx-32 prose-h4:prose-base dark:prose-invert prose-h1:mt-18 prose-h1:text-2xl md:prose-h1:text-3xl prose-h1:font-semibold prose-h1:tracking-tighter prose-h1:font-serif prose-h2:mt-12 prose-h2:scroll-m-20 prose-h2:text-lg prose-h2:font-medium prose-h3:text-base prose-h3:font-medium prose-h4:font-medium prose-h5:text-base prose-h5:font-medium prose-h6:text-base prose-h6:font-medium prose-strong:font-medium">
+      <main className="mb-52 prose prose-gray md:prose-figure:-mx-18 lg:prose-figure:-mx-32 prose-h4:prose-base dark:prose-invert prose-h1:mt-18 prose-h1:text-2xl md:prose-h1:text-3xl prose-h1:font-semibold prose-h1:tracking-tighter prose-h1:font-serif prose-h2:mt-12 prose-h2:scroll-m-20 prose-h2:text-lg prose-h2:font-medium prose-h3:text-base prose-h3:font-medium prose-h4:font-medium prose-h5:text-base prose-h5:font-medium prose-h6:text-base prose-h6:font-medium prose-strong:font-medium prose-blockquote:my-16 prose-blockquote:mx-auto prose-blockquote:text-center prose-blockquote:border-none">
         {children}
       </main>
     </>
